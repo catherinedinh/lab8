7791bab4f50bae7425b78d426a1ea6a87db46081
Catherine Dinh <dinhcaatherine@gmail.com>
2021-02-20 16:26:46 -0800

    new fix

--- public/js/camera.js
+++ public/js/camera.js
@@ -1,8 +1,8 @@
 function initCamera() {
   // Uncomment and fill in the correct selectors below.
   capture($('#camera-video'),
-          $('#camera-button'),
-          $('#camera-canvas'));
+          $('#camera-canvas'),
+          $('#camera-button'));
 }
 
 function capture(video, canvas, snapshotButton) {
